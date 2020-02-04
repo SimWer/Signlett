c7916074dc2c10a23fd8b04b746028a7121989e3
Ove Simon Wernersson <simon.wernersson@live.no>
2020-02-04 13:28:30 +0100

    Add toastr

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,4 +2,12 @@ class DocumentsController < ApplicationController
   def show
   	@documents = Document.includes(:document_status).first
   end
+
+  def new
+  	@document = Document.new
+  end
+
+  def create
+  	
+  end
 end
